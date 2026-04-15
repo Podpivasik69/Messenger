d4e33a6bd21688f6f775e3a2e7943e8adc22b136
артемий <sk0opy@yandex.ru>
2026-04-15 17:44:38 +0300

    наметка базы данных

---
+++ .venv/data/users.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    username = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
