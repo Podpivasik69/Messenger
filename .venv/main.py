1a5412628cb342c661dc67c565aa76eb71494760
артемий <sk0opy@yandex.ru>
2026-04-15 18:10:38 +0300

    наброски веба

--- .venv/main.py
+++ .venv/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from data import db_session
+from data.users import User
+import WEB
 
 
 app = Flask(__name__)
@@ -8,6 +10,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/profiles.db")
+    WEB.website.run(port=8080, host='127.0.0.1')
     # app.run()
 
 
